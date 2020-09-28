ef8316f1becb671a038d4bfcdad082a43fb67719
Sands Fish <sandsfish@gmail.com>
2020-09-28 02:38:58 -0400

    Add smoothing; Simplify line width; Remove unused code; Fix even number bug with verts & lines

--- rtpAnniAlbers02/src/ofApp.cpp
+++ rtpAnniAlbers02/src/ofApp.cpp
@@ -3,6 +3,8 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    ofEnableAntiAliasing();
+    ofEnableSmoothing();
 }
 
 //--------------------------------------------------------------
@@ -15,20 +17,30 @@ void ofApp::draw(){
 
     ofBackground(206, 195, 165);
     ofSetColor(42, 41, 37);
-    ofSetLineWidth(1);
     
     ofDrawBitmapString("Sands Fish", 20, 30);
-    ofDrawBitmapString("After Bridget Riley", 20, 42);
-    ofDrawBitmapString(ofGetTimestampString("%c"), 20, 54);
+    ofDrawBitmapString("After Bridget Riley", 20, 44);
+    ofDrawBitmapString(ofGetTimestampString("%c"), 20, 58);
     
-    int gridX = 20;
-    int vertexCount = 11; //ofClamp(ofMap(ofGetMouseY(), 0, ofGetHeight(), 0, 100), 2, 200);
-    int lineCount = 13; //ofClamp(2+ofMap(mouseX, 0, ofGetWidth(), 0, 100), 2, 20);  //7;
-    int lineLength = 400;
+    int gridX = 2;
     
+    int vertexCount = ofClamp(ofMap(ofGetMouseY(), 0, ofGetHeight(), 0, 100), 2, 200); //11;
+    (vertexCount % 2 == 0) ? vertexCount = vertexCount+1 :  vertexCount;
+    
+    int lineCount = ofClamp(2+ofMap(ofGetMouseX(), 0, ofGetWidth(), 0, 100), 2, 20);  //13;
+    (lineCount % 2 == 0) ? lineCount = lineCount+1 :  lineCount;
+    
+    int lineLength = ofClamp(2+ofMap(ofGetMouseY(), 0, ofGetWidth(), 20, ofGetHeight()), 2, ofGetWidth());
+    
+    // Center drawing based on the width & height of the configured line/grid counts
     int shapeArrayWidth = (lineCount * gridX - gridX) / 2;
     int centeredOffsetX = ofGetWidth()/2 - shapeArrayWidth;
     int centeredOffsetY = ofGetHeight()/2 - lineLength / 2;
+    
+    int lineWidth = 3;
+    ofSetLineWidth(lineWidth);
+    
+    //--------------------------------------------------------------
     
     vector<ofPolyline> lines;
     
@@ -37,13 +49,13 @@ void ofApp::draw(){
     
         // Make parallel vertical polylines
         ofPolyline p;
-        p.addVertex( (sin(ofGetElapsedTimef())*60*x) + centeredOffsetX + x * gridX, centeredOffsetY );
-        p.addVertex( (sin(ofGetElapsedTimef()+1.5)*60*x) + centeredOffsetX + x * gridX, centeredOffsetY+lineLength );
+        p.addVertex( (sin(ofGetElapsedTimef()*0.65)*55*x) + centeredOffsetX + x * gridX, centeredOffsetY );
+        p.addVertex( (sin(ofGetElapsedTimef()*0.65+1.5)*55*x) + centeredOffsetX + x * gridX, centeredOffsetY+lineLength );
         p = p.getResampledByCount(vertexCount); // .getSmoothed(20);
         lines.push_back(p);
     }
     
-    ofSetLineWidth(1);
+    //--------------------------------------------------------------
     
     // Draw verticies as points and vertical polylines
     for(int i = 0; i < lines.size(); i++) {
@@ -69,8 +81,8 @@ void ofApp::draw(){
         }
     }
     
-    ofSetLineWidth(1);
-    
+    //--------------------------------------------------------------
+
     // Draw Zig-Zags, Starting By Looping Through Parallel Vertices
     for(int v = 0; v < vertexCount-1; v++) {
     
@@ -79,53 +91,29 @@ void ofApp::draw(){
         
             ofPolyline firstLine;
             firstLine = lines[0];
-            auto firstLineVerts = firstLine.getVertices();
             
             ofPolyline currentLine;
             currentLine = lines[i];
-            auto currentLineVerts = currentLine.getVertices();
             
             ofPolyline nextLine;
             nextLine = lines[i+1];
-            auto nextLineVerts = nextLine.getVertices();
-                
-            //ofSetColor(ofColor::blue);
             
             if(i % 2 == 0) {
                 // Going Down!
-                ofDrawLine(currentLine[v].x, currentLine[v].y, nextLineVerts.at(v+1).x, nextLineVerts.at(v+1).y);
+                ofDrawLine(currentLine[v].x, currentLine[v].y, nextLine[v+1].x, nextLine[v+1].y);
             } else {
                 // Going Up!
-                ofDrawLine(currentLine[v+1].x, currentLine[v+1].y, nextLineVerts[v].x, nextLineVerts[v].y);
+                ofDrawLine(currentLine[v+1].x, currentLine[v+1].y, nextLine[v].x, nextLine[v].y);
             }
             
-            // We're At The Last Line (left to right) and on an even vertex
+            // Close the beginning & end of the shape when we hit the last line on an odd numbered vertex
             if(i == lines.size()-2 && v % 2 == 1) {
-                
-                ofSetLineWidth(1);
-                //ofSetColor(ofColor::blue);
-                
-                // Close the end & beginning of the shape if we're at the end of a row
-                // For an even numbered vertical line:
-                if(v % 2 == 1) {
                     
-                    // Close End
-                    ofDrawLine(nextLine[v-1].x, nextLine[v-1].y, nextLine[v].x, nextLine[v].y);
-                    
-                    // Close Beginning
-                    ofDrawLine(firstLine[v-1].x, firstLine[v-1].y, firstLine[v].x, firstLine[v].y);
+                // Close Beginning
+                ofDrawLine(firstLine[v-1].x, firstLine[v-1].y, firstLine[v].x, firstLine[v].y);
                 
-                // For an odd numbered vertical line (balanced):
-//                } else {
-//
-//                    if(v+2 < vertexCount-3) {
-//
-//                        ofSetLineWidth(3);
-//                        ofSetColor(ofColor::red);
-//                        ofDrawLine(nextLine[v+1].x, nextLine[v+1].y, nextLine[v+2].x, nextLine[v+2].y);
-//                        ofSetLineWidth(1);
-//                    }
-                }
+                // Close End
+                ofDrawLine(nextLine[v-1].x, nextLine[v-1].y, nextLine[v].x, nextLine[v].y);
             }
         }
     }
